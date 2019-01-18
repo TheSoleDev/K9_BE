3e53f9cc096ab87dae880b3646c6b52bcace28a6
Julius Garcia <garciajuliusb@yahoo.com>
2019-01-18 18:16:15 +0800

    with Login API - some dependency issue

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -15,97 +15,107 @@ use Symfony\Component\Form\FormInterface;
 
 class RegistrationController extends BaseController
 {
+    use \AppBundle\Helper\ControllerHelper;
+
     /**
      * @Route("/register", name="user_register")
      * @Method("POST")
      */
     public function registerAction(Request $request)
     {
-        /** @var \FOS\UserBundle\Form\Factory\FactoryInterface */
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
         $formFactory = $this->get('fos_user.registration.form.factory');
-        /** @var \FOS\UserBundle\Model\UserManagerInterface */
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
         $userManager = $this->get('fos_user.user_manager');
-        /** @var \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
         $dispatcher = $this->get('event_dispatcher');
-   
+
         $user = $userManager->createUser();
+        $user->setEnabled(true);
+
         $event = new GetResponseUserEvent($user, $request);
         $dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
- 
+
         if (null !== $event->getResponse()) {
             return $event->getResponse();
         }
 
         $form = $formFactory->createForm(array('csrf_protection' => false));
-
         $form->setData($user);
-
         $this->processForm($request, $form);
-        // echo $errors = (string) $form->getErrors(true, false);
-        // die($form); 
+
         if ($form->isValid()) {
-    
             $event = new FormEvent($form, $request);
-            $dispatcher->dispatch(
-                          FOSUserEvents::REGISTRATION_SUCCESS, $event
-                       );
- 
+            $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
 
             $userManager->updateUser($user);
- 
+
             $response = new Response($this->serialize('User created.'), Response::HTTP_CREATED);
-                die($response);
+        } else {
+            throw $this->throwApiProblemValidationException($form);
         }
 
         return $this->setBaseHeaders($response);
     }
- 
+
     /**
-     * @param  Request $request
-     * @param  FormInterface $form
+     * @param Request $request
+     * @param FormInterface $form
      */
     private function processForm(Request $request, FormInterface $form)
     {
-
         $data = json_decode($request->getContent(), true);
-
-
         if ($data === null) {
-        
             throw new BadRequestHttpException();
         }
- 
+
         $form->submit($data);
     }
- 
+
     /**
-     * Data serializing via JMS serializer.
+     * Returns response in case of invalid request.
      *
-     * @param mixed $data
+     * @param FormInterface $form
      *
-     * @return string JSON string
+     * @return ApiProblemException
+     * @todo Make custom response for invalid request
      */
-    private function serialize($data)
+    private function throwApiProblemValidationException(FormInterface $form)
     {
-        $context = new SerializationContext();
-        $context->setSerializeNull(true);
- 
-        return $this->get('jms_serializer')
-            ->serialize($data, 'json', $context);
+        $errors = $this->getErrorsFromForm($form);
+
+        throw new BadRequestHttpException($this->serialize($errors));
     }
- 
+
     /**
-     * Set base HTTP headers.
+     * Returns form errors.
      *
-     * @param Response $response
+     * @param FormInterface $form
      *
-     * @return Response
+     * @return array
      */
-    private function setBaseHeaders(Response $response)
+    private function getErrorsFromForm(FormInterface $form)
     {
-        $response->headers->set('Content-Type', 'application/json');
-        $response->headers->set('Access-Control-Allow-Origin', '*');
- 
-        return $response;
+        $errors = [];
+
+        foreach ($form->getErrors() as $key => $error) {
+            $template = $error->getMessageTemplate();
+            $parameters = $error->getMessageParameters();
+
+            foreach ($parameters as $var => $value) {
+                $template = str_replace($var, $value, $template);
+            }
+            $errors[$key] = $template;
+        }
+
+        foreach ($form->all() as $childForm) {
+            if ($childForm instanceof FormInterface) {
+                if ($childErrors = $this->getErrorsFromForm($childForm)) {
+                    $errors[$childForm->getName()] = $childErrors;
+                }
+            }
+        }
+
+        return $errors;
     }
-}    
+}
