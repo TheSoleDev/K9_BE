9800d27e2644e538d49e2e7818777b52fe8bddef
Julius Garcia <garciajuliusb@yahoo.com>
2019-01-22 16:54:00 +0800

    WITH CORS FIXES

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,8 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
-            new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),            
+            new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),   
+            new Nelmio\CorsBundle\NelmioCorsBundle(),         
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
